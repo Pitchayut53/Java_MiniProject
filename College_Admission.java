b0b3ca358014abf7225ca0f5e2561b07383bb9a3
Pitchayut <68723901+Pitchayut53@users.noreply.github.com>
2020-10-05 10:37:55 +0700

    Add files via upload

--- College_Admission.java
+++ College_Admission.java
@@ -1,13 +1,13 @@
 import java.util.*; 
-
 class College_Admission{
+static int num;
 public static void main(String args[]){
 Regis obj[] = new Regis[10];
 int d = 0;
 while (d==0){
 System.out.print("\n\n********** College Admission Management System **********\n\n");
 System.out.print("1. Enter Application Form\n");
-System.out.print("2. ***\n");
+System.out.print("2. Display All Application Form\n");
 System.out.print("3. Exit\n");
 System.out.print("Enter the option : ");
 Scanner ch= new Scanner(System.in);
@@ -15,13 +15,20 @@ int c = ch.nextInt();
 switch(c){
 	
 		case 1 :System.out.print("How many form you want to enter : ");
-			int n = ch.nextInt();
-			for(int i=0;i<n;i++)
-			{ 
-			obj[n].Register();
+			num = ch.nextInt();
+			for(int i=0;i<num;i++)
+			{
+			obj[num].Register();
 			}
+			for(int i=0;i<num;i++)
+			{
+			  obj[num].Display();
+			 }
 			break;
-		case 2 :
+		case 2 :for(int i=0;i<num;i++)
+			{
+			  obj[num].Display(1);
+			 }
 			break;
 		
 		case 3 :
@@ -40,23 +47,43 @@ switch(c){
 }
 
 class Regis{
+    static int studentId,ph;
+    static String fname,lname,email;
+    static String University = "Christ University";
 public static void Register(){
 Scanner ct= new Scanner(System.in);
 Scanner nu= new Scanner(System.in);
+Random rand = new Random(); 
+studentId  = rand.nextInt(1000);
 System.out.print("\n\n********** Application Form **********\n\n");
 System.out.print("\nEnter first name : ");
-String fname= ct.nextLine(); 
+fname= ct.nextLine(); 
 System.out.print("\nEnter last name : ");
-String lname= ct.nextLine(); 
+lname= ct.nextLine(); 
 System.out.print("\nEnter Email : ");
-String email= ct.nextLine(); 
+email= ct.nextLine(); 
 System.out.print("\nEnter phone number : ");
-int ph = nu.nextInt();
+ph = nu.nextInt();
   
+
+}
+
 //Display
+public static void Display(){
 System.out.println("\n\n********** Display Application Form **********\n\n");
+System.out.println("Student Id : "+studentId);
 System.out.println("\nStudent Name : "+fname+" "+lname);
 System.out.println("\nPhone number : "+ph);
 System.out.println("\nStudent email : "+email);    
+}
+
+//function overloading
+public static void Display(int n){
+System.out.println("\n\n********** Display Application Form **********\n\n");
+System.out.println("Student Id : "+studentId);
+System.out.println("\nStudent Name : "+fname+" "+lname);
+System.out.println("\nPhone number : "+ph);
+System.out.println("\nStudent email : "+email);  
+System.out.println("\nUniversity : "+University);    
 }
 }
