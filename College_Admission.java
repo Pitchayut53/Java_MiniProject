32beb95b8b55f5e244239cbd407d1cfc9d355115
Pitchayut <68723901+Pitchayut53@users.noreply.github.com>
2020-10-12 17:25:04 +0700

    Add files via upload

--- College_Admission.java
+++ College_Admission.java
@@ -1,89 +1,85 @@
 import java.util.*; 
-class College_Admission{
-static int num;
-public static void main(String args[]){
-Regis obj[] = new Regis[10];
-int d = 0;
-while (d==0){
-System.out.print("\n\n********** College Admission Management System **********\n\n");
-System.out.print("1. Enter Application Form\n");
-System.out.print("2. Display All Application Form\n");
-System.out.print("3. Exit\n");
-System.out.print("Enter the option : ");
-Scanner ch= new Scanner(System.in);
-int c = ch.nextInt(); 
-switch(c){
-	
-		case 1 :System.out.print("How many form you want to enter : ");
-			num = ch.nextInt();
-			for(int i=0;i<num;i++)
-			{
-			obj[num].Register();
-			}
-			for(int i=0;i<num;i++)
-			{
-			  obj[num].Display();
-			 }
-			break;
-		case 2 :for(int i=0;i<num;i++)
-			{
-			  obj[num].Display(1);
-			 }
-			break;
-		
-		case 3 :
-			System.out.print("\nHello World");
-			d = 1;
-			break;
-		default :
-			System.out.println("\nWrong input!!"); 
-		
+/**
+ * Write a description of Admission here.
+ * 
+ * @author (Pitchayut Khumkrongsap) 
+ * @version (10/12/2020)
+ */
 
-
-
-}
-}
-}
-}
-
-class Regis{
-    static int studentId,ph;
-    static String fname,lname,email;
+class Register {
+    int studentId,ph;
+    String name,email;
     static String University = "Christ University";
-public static void Register(){
-Scanner ct= new Scanner(System.in);
-Scanner nu= new Scanner(System.in);
-Random rand = new Random(); 
-studentId  = rand.nextInt(1000);
-System.out.print("\n\n********** Application Form **********\n\n");
-System.out.print("\nEnter first name : ");
-fname= ct.nextLine(); 
-System.out.print("\nEnter last name : ");
-lname= ct.nextLine(); 
-System.out.print("\nEnter Email : ");
-email= ct.nextLine(); 
-System.out.print("\nEnter phone number : ");
-ph = nu.nextInt();
-  
+    Scanner obj2 = new Scanner(System.in);
+    public void read()
+    {
+        Random rand = new Random(); 
+        studentId  = rand.nextInt(1000);
+        System.out.print("Enter Your Name : ");
+        this.name = obj2.nextLine();
+        System.out.print("Enter Your email : ");
+        this.email = obj2.nextLine();
+        System.out.print("Enter Your Phone number : ");
+        this.ph = obj2.nextInt();
+    }
+    public void display()
+    {
+        System.out.println("\nStudent Id is : "+this.studentId);
+        System.out.println("Student Name is : " + this.name);
+        System.out.println("Student email is : " + this.email); 
+        System.out.println("Student phone number is : " + this.ph); 
+        System.out.println("University : " + this.University); 
+    }
+}
+
+public class Admission {
+    
+   public static void main(String args[])
+   {
+       Register[] obj = new Register[10];
+       int d = 0;
+       while (d==0)
+       {
+        System.out.print("\n\n=================== College Admission Management System ===================\n");
+        System.out.print("===========================================================================\n\n");
+        System.out.print("1. Enter Application Form\n");
+        System.out.print("2. \n");
+        System.out.print("3. Exit\n");
+        System.out.print("Enter the option : ");
+        Scanner ch= new Scanner(System.in);
+        int c = ch.nextInt(); 
+        
+        switch(c)
+        {
+            case 1 :    System.out.print("Enter Number of Students: ");
+                        Scanner ns = new Scanner(System.in);
+                        int nos = Integer.parseInt(ns.nextLine());
+                        Register[] gobj = new Register[nos];
+                        for (int i=0;i<nos;i++){
+                            gobj[i] = new Register();
+                            System.out.printf("\n========== Enter %d Student Details ==========\n",i+1);
+                            gobj[i].read();
+                            } 
+                        for(Register n:gobj){
+                            n.display();
+                        }
+                        
+            
+            
+            case 2 : 
+            break;
+            
+            case 3 :
+            System.out.print("\nThank you");
+            d = 1;
+            break;
+            default :
+            System.out.println("\nWrong input!!");
+        }
+       }
+   }
+    
 
 }
 
-//Display
-public static void Display(){
-System.out.println("\n\n********** Display Application Form **********\n\n");
-System.out.println("Student Id : "+studentId);
-System.out.println("\nStudent Name : "+fname+" "+lname);
-System.out.println("\nPhone number : "+ph);
-System.out.println("\nStudent email : "+email);    
-}
 
-//function overloading
-public static void Display(int n){
-System.out.println("\n\n********** Display Application Form **********\n\n");
-System.out.println("Student Id : "+studentId);
-System.out.println("\nStudent Name : "+fname+" "+lname);
-System.out.println("\nPhone number : "+ph);
-System.out.println("\nStudent email : "+email);  
-System.out.println("\nUniversity : "+University);    
-}
-}
