0e618da2abd064947f12d5b8d334517c9bd14db2
Pitchayut <68723901+Pitchayut53@users.noreply.github.com>
2020-11-23 15:34:28 +0700

    Add files via upload

--- College_Admission.java
+++ College_Admission.java
@@ -6,10 +6,17 @@ import java.util.*;
  * @version (10/12/2020)
  */
 
-class Register {
+interface Testinterface
+{
+static String University = "Christ University";
+
+}
+
+class Register implements Testinterface {
     int studentId,ph;
-    String name,email;
-    static String University = "Christ University";
+    double gpa;
+    String name,email;    
+    StringBuffer course = new StringBuffer("Course"); 
     Scanner obj2 = new Scanner(System.in);
     public void read()
     {
@@ -24,6 +31,7 @@ class Register {
     }
     public void display()
     {
+        System.out.print("\n\n=================== Display Student Details ===================\n");
         System.out.println("\nStudent Id is : "+this.studentId);
         System.out.println("Student Name is : " + this.name);
         System.out.println("Student email is : " + this.email); 
@@ -32,19 +40,58 @@ class Register {
     }
 }
 
-public class Admission {
+class Courses extends Register {
+
+    public void select()
+    {
+        System.out.print("1. BCA\n");
+        System.out.print("2. BSC\n");
+        System.out.print("3. MSC\n");
+        System.out.print("Enter the option : ");
+        Scanner ch= new Scanner(System.in);
+        int c = ch.nextInt(); 
+        if(c == 1)
+        {
+        this.course = course.append(" BCA");
+        System.out.println("\nYou've selected : ");
+        System.out.println(course);
+        }
+        else if(c == 2)
+        {
+        this.course = course.append(" BSC");
+        System.out.println("\nYou've selected : ");
+        System.out.println(course);
+        }
+        else if(c == 3)
+        {
+        this.course = course.append(" MSC");
+        System.out.println("\nYou've selected : ");
+        System.out.println(course);
+        }
+        else
+        {
+            System.out.print("\nWrong input!!");
+        }   
+    }
     
+}
+
+public class College_Admission {
+    static int nos;
    public static void main(String args[])
    {
-       Register[] obj = new Register[10];
+       //Courses[] gobj = new Courses[10];
+      //Courses saveval[] = null; 
        int d = 0;
+       String testex = null; 
        while (d==0)
        {
         System.out.print("\n\n=================== College Admission Management System ===================\n");
         System.out.print("===========================================================================\n\n");
-        System.out.print("1. Enter Application Form\n");
-        System.out.print("2. \n");
-        System.out.print("3. Exit\n");
+        System.out.print("1. Enter Application Form / Display Application Form\n");
+        System.out.print("2. Select Course\n");
+        System.out.print("3. Exception Handling Test\n");
+        System.out.print("4. Exit\n");
         System.out.print("Enter the option : ");
         Scanner ch= new Scanner(System.in);
         int c = ch.nextInt(); 
@@ -53,23 +100,43 @@ public class Admission {
         {
             case 1 :    System.out.print("Enter Number of Students: ");
                         Scanner ns = new Scanner(System.in);
-                        int nos = Integer.parseInt(ns.nextLine());
-                        Register[] gobj = new Register[nos];
+                        nos = Integer.parseInt(ns.nextLine());
+                        Courses[] gobj = new Courses[nos];
                         for (int i=0;i<nos;i++){
-                            gobj[i] = new Register();
+                            gobj[i] = new Courses();
                             System.out.printf("\n========== Enter %d Student Details ==========\n",i+1);
                             gobj[i].read();
                             } 
-                        for(Register n:gobj){
+                        for(Courses n:gobj){
                             n.display();
                         }
                         
             
-            
-            case 2 : 
+            break;
+            case 2 :    gobj = new Courses[nos];
+                        for (int i=0;i<nos;i++){
+                            gobj[i] = new Courses();
+                            System.out.printf("\n========== %d Student Selects Courses ==========\n",i+1);
+                            gobj[i].select();
+                            } 
             break;
             
-            case 3 :
+            case 3 : 
+             try
+                { 
+                    // This line of code throws NullPointerException 
+                    if (testex.equals("Hello")) 
+                        System.out.print("Same"); 
+                    else 
+                        System.out.print("Not Same"); 
+                } 
+                catch(NullPointerException e) 
+                { 
+                    System.out.print("NullPointerException Caught"); 
+                } 
+              
+            break;           
+            case 4 :
             System.out.print("\nThank you");
             d = 1;
             break;
@@ -78,8 +145,8 @@ public class Admission {
         }
        }
    }
+}
     
 
-}
 
 
